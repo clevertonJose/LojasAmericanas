6bfc29d54280b7304bbe1b0b1d1ad4bdd3e74e37
cleverton josé <clevertonkel123@gmail.com>
2019-08-08 16:23:55 -0300

    Adição dos atributos da classe dependente e seus getters e setters

--- src/modelo/Dependente.java
+++ src/modelo/Dependente.java
@@ -2,4 +2,41 @@ package modelo;
 
 public class Dependente {
 
+	private int codigo;
+	private String nome;
+	private Funcionario funcionario;
+	private String dataDeNascimento;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+	
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+	public String getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+	
+	public void setDataDeNascimento(String dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+		
 }
