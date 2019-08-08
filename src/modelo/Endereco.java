0ebc43ebb1545908d8fc96548807e157037dd416
cleverton josé <clevertonkel123@gmail.com>
2019-08-08 15:42:11 -0300

    Criação da classe Endereço e adição dos atributos e dos metodos getters e setters

---
+++ src/modelo/Endereco.java
@@ -0,0 +1,50 @@
+package modelo;
+
+public class Endereco {
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String logradouro;
+	private int numero;
+	
+	public String getRua() {
+		return rua;
+	}
+	
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	
+	public String getBairro() {
+		return bairro;
+	}
+	
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+}
