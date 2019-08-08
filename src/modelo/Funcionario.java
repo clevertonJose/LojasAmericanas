225d9c6bdb5ab5ba14bc822c5f7fe3c06d416dbd
cleverton josé <clevertonkel123@gmail.com>
2019-08-08 16:11:40 -0300

    adição dos atributos da classe Funcionario e de seus getters e setters

--- src/modelo/Funcionario.java
+++ src/modelo/Funcionario.java
@@ -1,5 +1,107 @@
 package modelo;
 
+import java.util.List;
+
 public class Funcionario {
 
+	private int matricula;
+	private String cpf;
+	private String rg;
+	private String nome;
+	private Endereco endereco;
+	private int grauDeInstrucao;
+	private List<Dependente> dependentes;
+	private String dataDeNascimento;
+	private String dataDeAdmissao;
+	private double salario;
+	private String cargo;
+	
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getRg() {
+		return rg;
+	}
+	
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	public int getGrauDeInstrucao() {
+		return grauDeInstrucao;
+	}
+	
+	public void setGrauDeInstrucao(int grauDeInstrucao) {
+		this.grauDeInstrucao = grauDeInstrucao;
+	}
+	
+	public List<Dependente> getDependentes() {
+		return dependentes;
+	}
+	
+	public void setDependentes(List<Dependente> dependentes) {
+		this.dependentes = dependentes;
+	}
+
+	public String getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(String dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public String getDataDeAdmissao() {
+		return dataDeAdmissao;
+	}
+
+	public void setDataDeAdmissao(String dataDeAdmissao) {
+		this.dataDeAdmissao = dataDeAdmissao;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public String getCargo() {
+		return cargo;
+	}
+	
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}	
 }
